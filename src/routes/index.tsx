959e7390a5ed4d752101beafe98cb85c07731729
LeoMSSilva <l.eom1994@hotmail.com>
2021-06-26 04:38:14 -0300

    feat: Data persist and share invites

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { AuthRoutes } from './auth.routes';
+import { AuthRoutes } from './app.routes';
 import { SignIn } from '../screens/SignIn';
 import { useAuth } from '../hooks/auth';
 
