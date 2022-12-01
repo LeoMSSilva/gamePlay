a18eaee1e7f931f388ef20d23154bfebc3434006
LeoMSSilva <l.eom1994@hotmail.com>
2022-12-01 14:28:32 -0300

    build: installed and configured dependencies to write relative paths

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,18 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['inline-dotenv'],
+    plugins: [
+      'inline-dotenv',
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+          alias: {
+            '~': './src/',
+          },
+        },
+      ],
+    ],
   };
 };
